4ef49f67404a0a2ec76365cede7071ef497e3b61
Jose Cassano <cassanojoseluis@gmail.com>
2024-07-24 19:15:53 -0300

    add sql scripts for migration sequences set last sequence values

--- back/src/main/resources/db/migration/U1_18__sequences_changes.sql
+++ back/src/main/resources/db/migration/U1_18__sequences_changes.sql
@@ -62,3 +62,41 @@ ALTER TABLE users
     ALTER COLUMN id SET DEFAULT nextval('hibernate_sequence');
 
 ALTER SEQUENCE hibernate_sequence OWNED BY users.id;
+
+-- Script to migrate all sequences to unique sequence hibernate_sequence getting max value from all sequences
+DO $$
+DECLARE
+    max_last_value bigint;
+BEGIN
+    BEGIN
+        -- Retrieve the value for init hibernate_sequence
+        SELECT MAX(last_value) INTO max_last_value
+        FROM (
+                 SELECT last_value FROM assigned_slot_id_seq
+                 UNION ALL
+                 SELECT last_value FROM document_id_seq
+                 UNION ALL
+                 SELECT last_value FROM open_space_id_seq
+                 UNION ALL
+                 SELECT last_value FROM review_id_seq
+                 UNION ALL
+                 SELECT last_value FROM room_id_seq
+                 UNION ALL
+                 SELECT last_value FROM slot_id_seq
+                 UNION ALL
+                 SELECT last_value FROM talk_id_seq
+                 UNION ALL
+                 SELECT last_value FROM track_id_seq
+                 UNION ALL
+                 SELECT last_value FROM users_id_seq
+         ) AS all_sequences;
+    EXCEPTION
+        WHEN others THEN
+            max_last_value := 1;
+    END;
+
+
+    -- set the max value of all entities sequences
+    EXECUTE format('ALTER SEQUENCE IF EXISTS hibernate_sequence RESTART WITH %s', max_last_value);
+
+END $$;
--- back/src/main/resources/db/migration/V1_18__sequences_changes.sql
+++ back/src/main/resources/db/migration/V1_18__sequences_changes.sql
@@ -69,3 +69,28 @@ ALTER TABLE users
     ALTER COLUMN id SET DEFAULT nextval('users_id_seq');
 
 ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+-- Script to migrate the last sequence value from hibernate_sequence to the new sequences
+DO $$
+DECLARE
+    last_seq_value bigint;
+BEGIN
+    BEGIN
+        SELECT last_value INTO last_seq_value FROM hibernate_sequence;
+    EXCEPTION
+            WHEN others THEN
+                last_seq_value := 1;
+    END;
+
+    EXECUTE format('ALTER SEQUENCE IF EXISTS assigned_slot_id_seq RESTART WITH %s', last_seq_value);
+    EXECUTE format('ALTER SEQUENCE IF EXISTS document_id_seq RESTART WITH %s', last_seq_value);
+    EXECUTE format('ALTER SEQUENCE IF EXISTS open_space_id_seq RESTART WITH %s', last_seq_value);
+    EXECUTE format('ALTER SEQUENCE IF EXISTS review_id_seq RESTART WITH %s', last_seq_value);
+    EXECUTE format('ALTER SEQUENCE IF EXISTS room_id_seq RESTART WITH %s', last_seq_value);
+    EXECUTE format('ALTER SEQUENCE IF EXISTS slot_id_seq RESTART WITH %s', last_seq_value);
+    EXECUTE format('ALTER SEQUENCE IF EXISTS talk_id_seq RESTART WITH %s', last_seq_value);
+    EXECUTE format('ALTER SEQUENCE IF EXISTS track_id_seq RESTART WITH %s', last_seq_value);
+    EXECUTE format('ALTER SEQUENCE IF EXISTS users_id_seq RESTART WITH %s', last_seq_value);
+END $$;
+
+DROP SEQUENCE IF EXISTS hibernate_sequence CASCADE;

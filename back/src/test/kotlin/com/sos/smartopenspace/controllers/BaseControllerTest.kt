77cb11e1d0a92f4a432881d2277d5dafba5362bf
Jose Cassano <39202199+cassa10@users.noreply.github.com>
2024-10-29 20:58:12 -0300

    Revert "Revert "enhancement/270-improve-observability""

---
+++ back/src/test/kotlin/com/sos/smartopenspace/controllers/BaseControllerTest.kt
@@ -0,0 +1,15 @@
+package com.sos.smartopenspace.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class BaseControllerTest {
+    @Autowired
+    protected lateinit var mockMvc: MockMvc
+}

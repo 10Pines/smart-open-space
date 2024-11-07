e948221b73af33f434515d2b247d240ae30440ca
Jose Cassano <cassanojoseluis@gmail.com>
2024-11-06 21:52:06 -0300

    delete EnableAspectJAutoProxy and fix gradle build task

--- back/src/main/kotlin/com/sos/smartopenspace/SmartOpenSpaceApplication.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/SmartOpenSpaceApplication.kt
@@ -2,10 +2,8 @@ package com.sos.smartopenspace
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 class SmartOpenSpaceApplication
 
 fun main(args: Array<String>) {

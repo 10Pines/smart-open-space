08cac5966d9216c29228c3d0ff8bec5fc96b5476
Jose Cassano <cassanojoseluis@gmail.com>
2024-11-06 21:24:54 -0300

    fix fun doc

--- back/src/main/kotlin/com/sos/smartopenspace/aspect/LoggingAnnotations.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/aspect/LoggingAnnotations.kt
@@ -11,8 +11,8 @@ annotation class LoggingExecution
 
 /**
  * This annotation enable to log at two points:
- * - before execute with the input of method and after result log if success
- * - after execute if method success WITHOUT result
+ * - BEFORE execute with the input of method and after result log if success
+ * - AFTER execute if method success WITHOUT result
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)

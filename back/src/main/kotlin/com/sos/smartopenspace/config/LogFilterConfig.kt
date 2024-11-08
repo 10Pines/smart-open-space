684cb34123acf14810fa02d909afbdda7bf96708
Jose Cassano <cassanojoseluis@gmail.com>
2024-11-08 20:16:27 -0300

    add X-Internal-Session-Id in headers at LogFilterConfig and log it in logback spring console format

--- back/src/main/kotlin/com/sos/smartopenspace/config/LogFilterConfig.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/config/LogFilterConfig.kt
@@ -13,7 +13,12 @@ class LogFilterConfig : OncePerRequestFilter() {
 
     companion object {
         private const val REQUEST_ID_HEADER = "X-Request-Id"
+        private const val REQUEST_ID_SESSION = "X-Internal-Session-Id"
+
         private const val CTX_REQUEST_ID_KEY = "requestId"
+        private const val CTX_REQUEST_SESSION_ID_KEY = "requestSessionId"
+
+        private const val DEFAULT_EMPTY_VALUE = "none"
     }
 
     override fun doFilterInternal(
@@ -24,7 +29,12 @@ class LogFilterConfig : OncePerRequestFilter() {
         val defaultRequestId = UUID.randomUUID().toString()
         val requestId = request.getHeader(REQUEST_ID_HEADER)?.ifBlank { defaultRequestId }
             ?: defaultRequestId
+
+        val requestSessionId = request.getHeader(REQUEST_ID_SESSION)?.ifBlank { DEFAULT_EMPTY_VALUE }
+            ?: DEFAULT_EMPTY_VALUE
+
         MDC.put(CTX_REQUEST_ID_KEY, requestId)
+        MDC.put(CTX_REQUEST_SESSION_ID_KEY, requestSessionId)
         filterChain.doFilter(request, response)
     }
 

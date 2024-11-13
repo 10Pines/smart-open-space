99312ff51a2c7e8bd8cf42d241b39e4dd2f2c660
nicovillamonte <Nicolas Villamonte>
2024-11-13 17:55:14 -0300

    feat: RoomPickerForm -> Changed the props to recieve a room and an onChange function

--- front/src/App/components/molecule/RoomPickerForm.jsx
+++ front/src/App/components/molecule/RoomPickerForm.jsx
@@ -2,29 +2,44 @@ import {Box, Text, Card as GrommetCard,} from "grommet";
 import Input from "../atom/Input.jsx";
 import customTheme from "#app/theme.js";
 
-const RoomPickerForm = ({ name, onChangeName, link, onChangeLink }) => {
-    return (
-        <GrommetCard style={{padding: '20px 10px 10px 10px', borderRadius: '10px'}}
-             background={customTheme.global.colors.primary.light}
-             direction={'column'}
-             width={"300px"}
-             gap={"small"}>
-            <Input
-                primary={false}
-                label={"Nombre de la sala"}
-                placeholder={"Ingrese el nombre de la sala"}
-                value={name}
-                onChange={onChangeName}
-            />
-            <Input
-                primary={false}
-                label={"Link sala virtual"}
-                placeholder={"Ingrese link a la sala virtual"}
-                value={link}
-                onChange={onChangeLink}
-            />
-        </GrommetCard>
-    )
-}
+const RoomPickerForm = ({
+  room = {
+    name: '',
+    link: '',
+  },
+  onChange,
+  ...props
+}) => {
+  const { name, link } = room;
+
+  const onChangeName = (name) => onChange({ ...room, name: name });
+  const onChangeLink = (link) => onChange({ ...room, link: link });
+
+  return (
+    <GrommetCard
+      style={{ padding: '20px 15px', borderRadius: '10px' }}
+      background={customTheme.global.colors.primary.light}
+      direction={'column'}
+      width={'300px'}
+      gap={'small'}
+      {...props}
+    >
+      <Input
+        primary={false}
+        label={'Nombre de la sala'}
+        placeholder={'Ingrese el nombre de la sala'}
+        value={name}
+        onChange={onChangeName}
+      />
+      <Input
+        primary={false}
+        label={'Link sala virtual'}
+        placeholder={'Ingrese link a la sala virtual'}
+        value={link}
+        onChange={onChangeLink}
+      />
+    </GrommetCard>
+  );
+};
 
 export default RoomPickerForm;

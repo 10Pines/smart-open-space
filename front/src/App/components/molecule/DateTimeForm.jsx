cbe71169383a1b0fd1d7e4475d2d40e46c796a57
nicovillamonte <Nicolas Villamonte>
2024-11-21 17:57:15 -0300

    feat: Added minDate and maxDate logic to the DateTimeForm

--- front/src/App/components/molecule/DateTimeForm.jsx
+++ front/src/App/components/molecule/DateTimeForm.jsx
@@ -4,17 +4,29 @@ import DateTimePicker from "../atom/DateTimePicker.jsx";
 
 const DateTimeForm = ({ title, value, onChange, ...props }) => {
     return (
-        <Box style={{backgroundColor: "#3F8880", borderRadius: '5px'}}
-             width={'390px'}
-             height={'130px'}
-             direction='column'
-             {...props}
-        >
-            <Text color={"white"} weight={'bold'} style={{padding: '10px 10px 10px 17px'}}>{title}</Text>
-            <Box alignSelf={'center'} margin={{top: 'xsmall'}}>
-                <DateTimePicker onChange={(date)=>{onChange(date)}} value={value} primary={true} alignSelf={"center"}/>
-            </Box>
+      <Box
+        style={{ backgroundColor: '#3F8880', borderRadius: '5px' }}
+        width={'390px'}
+        height={'130px'}
+        direction="column"
+        {...props}
+      >
+        <Text color={'white'} weight={'bold'} style={{ padding: '10px 10px 10px 17px' }}>
+          {title}
+        </Text>
+        <Box alignSelf={'center'} margin={{ top: 'xsmall' }}>
+          <DateTimePicker
+            onChange={(date) => {
+              onChange(new Date(date));
+            }}
+            value={value}
+            primary={true}
+            alignSelf={'center'}
+            minDate={props.minDate ? props.minDate : null}
+            maxDate={props.maxDate ? props.maxDate : null}
+          />
         </Box>
+      </Box>
     );
 };
 

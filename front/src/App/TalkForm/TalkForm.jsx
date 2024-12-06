52d2bba5313414ad564628ab03efb3eea1dd721d
azu26 <azulucila1@gmail.com>
2024-12-06 15:26:12 -0300

    wip

--- front/src/App/TalkForm/TalkForm.jsx
+++ front/src/App/TalkForm/TalkForm.jsx
@@ -1,9 +1,19 @@
 import { useHistory } from 'react-router-dom';
 import MainHeader from '#shared/MainHeader';
-import { TalkIcon, TextAreaIcon } from '#shared/icons';
+import {HomeIcon, TalkIcon, TextAreaIcon} from '#shared/icons';
 import MyForm from '#shared/MyForm';
 import React from 'react';
 import Documents from './Documents';
+import {Box, Text} from "grommet";
+import customTheme from "#app/theme.js";
+import Input from "#components/atom/Input.jsx";
+import SelectDropdown from "#components/atom/SelectDropdown.jsx";
+import FormTitle from "#components/atom/FormTitle.jsx";
+import Carousel from "#components/molecule/Carousel.jsx";
+import Badge from "#components/molecule/Badge.jsx";
+import RoomPickerForm from "#components/molecule/RoomPickerForm.jsx";
+import AddElementBox from "#components/molecule/AddElementBox.jsx";
+import Button from "#components/atom/Button.jsx";
 
 const emptyTalk = {
   name: '',
@@ -11,6 +21,12 @@ const emptyTalk = {
   meetingLink: '',
   track: undefined,
   documents: [],
+};
+
+const cardsAnimation = {
+  type: 'fadeIn',
+  delay: 1,
+  duration: 800,
 };
 
 export const TalkForm = ({
@@ -24,55 +40,82 @@ export const TalkForm = ({
   const history = useHistory();
   const nullTrackOption = { id: null, name: 'Sin track' }; // Customize the name as needed
   const trackOptionsWithNull = [nullTrackOption, ...(openSpace?.tracks || [])];
+  console.log('DOCUMENTS:', openSpace)
 
   return (
-    <>
-      <MainHeader>
-        <MainHeader.Title icon={TalkIcon} label={title} />
-        <MainHeader.SubTitle>{subtitle}</MainHeader.SubTitle>
-      </MainHeader>
-      <MyForm
-        onSecondary={history.goBack}
-        onSubmit={onSubmit}
-        initialValue={initialValues}
-      >
-        <MyForm.Text
+    <Box  pad={"10px"} style={{border: `1px solid ${customTheme.global.colors.primary.light}`, borderRadius: "5px"}}>
+
+      <Box direction={"row"} align={"center"} justify={"between"} margin={{bottom: "medium"}}>
+        <MainHeader.Title label={title} style={{fontSize: "1.6rem"}} />
+        <Box width={"30%"}>
+          <SelectDropdown
+              defaultMessage={"Seleccionar eje temático"}
+              name="track"
+              options={trackOptionsWithNull.map((option) => option.name)}
+              width={"30%"}
+          />
+        </Box>
+      </Box>
+
+      <Box direction={"column"} gap={"small"}>
+        <Input
           id="talk-title-id"
           label="Título"
-          formValueName="name"
-          placeholder="¿Como querés nombrar tu charla?"
+          placeholder="¿Cómo querés nombrar tu charla?"
         />
-        <MyForm.TextArea
-          style={{ fontFamily: 'monospace' }}
-          placeholder="Describí tu charla con mas detalle. Podés usar Markdown"
+        <Input
+            id="talk-title-description"
+            label="Descripción"
+            placeholder="Agregá la descripción acerca de lo que se va a tratar tu charla"
+            multiline
         />
-        <MyForm.Link label="Link" placeholder="Link a la reunión virtual (meet/zoom)" />
-        <MyForm.Select
-          label="Track"
-          name="track"
-          options={trackOptionsWithNull}
-          labelKey="name"
-          valueKey="id"
+        <Input
+            id="talk-title-link"
+            label="Link"
+            placeholder="Link a la reunión virtual (meet/zoom)"
         />
-        <MyForm.Field
-          component={Documents}
-          icon={<TextAreaIcon />}
-          label="Documentos"
-          name="documents"
-          labelKey="name"
-          valueKey="id"
-          required={false}
-        />
+
+        <Box>
+          <Text>Documentos</Text>
+          <Carousel >
+            {initialValues.documents &&
+                initialValues.documents.map((document, index) => (
+                    <Box
+                        style={{ minWidth: '300px', paddingTop: '0.5rem' }}
+                        animation={cardsAnimation}
+                    >
+                      <Badge key={index} onClick={() => {}}>
+                        <RoomPickerForm
+                            room={document}
+                            onChange={(roomChanged) => {}}
+                            width={{ min: '300px' }}
+                            animation={cardsAnimation}
+                        />
+                      </Badge>
+                    </Box>
+                ))}
+            <AddElementBox
+                onClick={()=>{}}
+                size={{
+                  height: initialValues.documents > 0 ? 'auto' : '146px',
+                  width: '300px',
+                }}
+                width={{ min: '300px' }}
+            />
+          </Carousel>
+        </Box>
         {amTheOrganizer && (
-          <MyForm.Text
-            id="talk-speaker-name"
-            label="Nombre del Orador"
-            name="speakerName"
-            placeholder="En caso de ser un orador que pitcheó en el marketplace, ingresa el nombre completo"
-            required={false}
-          />
+            <Input
+                id="talk-title-link"
+                label="Nombre del Orador"
+                placeholder="En caso de ser un orador que pitcheó en el marketplace, ingresa el nombre completo"
+            />
         )}
-      </MyForm>
-    </>
+        <Box direction={"row"} align={"center"} justify={"center"} gap={"large"} margin={{top: "medium"}}>
+          <Button style={{width: "10rem"}} secondary label={"Cancelar"}/>
+          <Button style={{width: "10rem"}} label={"Crear"}/>
+        </Box>
+      </Box>
+    </Box>
   );
 };

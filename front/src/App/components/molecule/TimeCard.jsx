c5e36a82eddebc5648b30d638762a1a49cb0d217
azu26 <azulucila1@gmail.com>
2024-12-17 11:01:23 -0300

    round corners adn add isOrganizer prop to Card child

--- front/src/App/components/molecule/TimeCard.jsx
+++ front/src/App/components/molecule/TimeCard.jsx
@@ -4,16 +4,20 @@ import { Box } from 'grommet';
 import PropTypes from 'prop-types';
 import {useState} from "react";
 import {usePushToOpenSpace} from "#helpers/routes.jsx";
+import {useUser} from "#helpers/useAuth.jsx";
 
-const TimeCard = ({ time, dates, id, ...props }) => {
+const TimeCard = ({ time, dates, id, author, ...props }) => {
     const [hover, setHover] = useState(false);
     const PushToOpenSpace = usePushToOpenSpace(id);
+    const user = useUser();
+    const isOrganizer = author.id === user.id;
   return (
     <Box direction="row" width={"580px"} height={"300px"} onClick={() => {PushToOpenSpace()}}
          borderColor={hover ? 'accent-1' : 'brand'}
          elevation={hover ? 'xlarge' : 'small'}
          onMouseEnter={() => setHover(true)}
          onMouseLeave={() => setHover(false)}
+         round={{size: '5px'}}
     >
       <Box
         round={{
@@ -48,6 +52,7 @@ const TimeCard = ({ time, dates, id, ...props }) => {
           size: '5px',
           corner: 'right',
         }}
+        isOrganizer={isOrganizer}
         color="white"
       />
     </Box>

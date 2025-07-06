fd969ccc1ce6d854d21ce926c37db4370ea7b6b9
Jose Cassano <cassanojoseluis@gmail.com>
2025-07-06 20:59:46 -0300

    Add logs when get cors props and delete private ip in app props cors

--- back/src/main/kotlin/com/sos/smartopenspace/config/CorsProps.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/config/CorsProps.kt
@@ -1,5 +1,6 @@
 package com.sos.smartopenspace.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
@@ -12,10 +13,28 @@ class CorsProps(
     @Value("\${app.cors.allowed.socket-origins}")
     val corsAllowedSocketOrigins: String,
 ) {
-    fun corsAllowedMethods(): Array<String> = getArrayFromStringRaw(corsAllowedMethods)
-    fun corsAllowedOrigins(): Array<String> = getArrayFromStringRaw(corsAllowedOrigins)
-    fun corsSocketOrigins(): Array<String> = getArrayFromStringRaw(corsAllowedSocketOrigins)
+
+    fun corsAllowedMethods(): Array<String> = getArrayFromStringRaw(corsAllowedMethods).also {
+        logArrayValue("Allowed CORS methods", it)
+    }
+
+    fun corsAllowedOrigins(): Array<String> = getArrayFromStringRaw(corsAllowedOrigins).also {
+        logArrayValue("Allowed CORS origins", it)
+    }
+
+    fun corsSocketOrigins(): Array<String> = getArrayFromStringRaw(corsAllowedSocketOrigins).also {
+        logArrayValue("Allowed CORS socket origins", it)
+    }
 
     private fun getArrayFromStringRaw(stringRaw: String): Array<String> =
         stringRaw.split(",").map { it.trim() }.toTypedArray()
+
+    private fun logArrayValue(header: String, value: Array<String>) {
+        LOGGER.info("${header}: ${value.joinToString(", ")}")
+    }
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(this::class.java)
+    }
+
 }

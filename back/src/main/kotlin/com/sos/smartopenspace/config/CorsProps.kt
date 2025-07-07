f1c656cbfd92d8800f7053603472a359b8b8bec0
Jose Cassano <39202199+cassa10@users.noreply.github.com>
2025-07-07 18:47:34 -0300

    Enhancement/444 cd GitHub kamal (#445)
    * Add proxy with ssl true in back n front deploy ymls and remove if's in github actions to test CD
    
    * increase services deploy timeout to 1min
    
    * set up ssl_redirect false to avoid loop redirects
    
    * Setup redirect ssl false in front yml too
    
    * fix cd front action
    
    * unify cd deploys actions workflows
    
    * Revert "unify cd deploys actions workflows"
    
    This reverts commit 79441bec8bf6ddf6558eaa97f58d2ba43c8bb3f8.
    
    * change cd front workflow run with job name and add if's
    
    * Set up cd front action with correct name workflow
    
    * test cd front
    
    * rename CD front action
    
    * remove if in cd back
    
    * remove branches in cd front
    
    * Unify CD back and front in one yml
    
    * Fix api_url deploy config front
    
    * Fix deploy_back job and update kamal documentation
    
    * Add logs when get cors props and delete private ip in app props cors
    
    * Add 2 vars to enable job step, add cors logs in back, update kamal docs
    
    * Remove webConfig Profile annotation
    
    * Set up final cd action

--- back/src/main/kotlin/com/sos/smartopenspace/config/CorsProps.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/config/CorsProps.kt
@@ -1,5 +1,6 @@
 package com.sos.smartopenspace.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
@@ -12,10 +13,35 @@ class CorsProps(
     @Value("\${app.cors.allowed.socket-origins}")
     val corsAllowedSocketOrigins: String,
 ) {
-    fun corsAllowedMethods(): Array<String> = getArrayFromStringRaw(corsAllowedMethods)
-    fun corsAllowedOrigins(): Array<String> = getArrayFromStringRaw(corsAllowedOrigins)
-    fun corsSocketOrigins(): Array<String> = getArrayFromStringRaw(corsAllowedSocketOrigins)
+    init {
+        LOGGER.info("CorsProps=$this")
+    }
+
+    fun corsAllowedMethods(): Array<String> = getArrayFromStringRaw(corsAllowedMethods).also {
+        logArrayValue("Allowed CORS methods", it)
+    }
+
+    fun corsAllowedOrigins(): Array<String> = getArrayFromStringRaw(corsAllowedOrigins).also {
+        logArrayValue("Allowed CORS origins", it)
+    }
+
+    fun corsSocketOrigins(): Array<String> = getArrayFromStringRaw(corsAllowedSocketOrigins).also {
+        logArrayValue("Allowed CORS socket origins", it)
+    }
 
     private fun getArrayFromStringRaw(stringRaw: String): Array<String> =
         stringRaw.split(",").map { it.trim() }.toTypedArray()
+
+    private fun logArrayValue(header: String, value: Array<String>) {
+        LOGGER.info("${header}: ${value.joinToString(", ")}")
+    }
+
+    override fun toString() =
+        "CorsProps(corsAllowedMethods='$corsAllowedMethods', corsAllowedOrigins='$corsAllowedOrigins', corsAllowedSocketOrigins='$corsAllowedSocketOrigins')"
+
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(this::class.java)
+    }
+
 }
--- back/src/main/kotlin/com/sos/smartopenspace/config/WebConfig.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/config/WebConfig.kt
@@ -1,12 +1,10 @@
 package com.sos.smartopenspace.config
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@Profile("dev", "docker")
 class WebConfig(
   private val corsProps: CorsProps
 ) : WebMvcConfigurer {

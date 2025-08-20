c2808ed63c039c88ffeff000c3c60efed1a842b5
Jose Cassano <cassanojoseluis@gmail.com>
2025-08-19 23:00:41 -0300

    fix bug unhandled fallback exceptions returning 200 status code, add exception handlers and add exception handler tests

--- back/src/main/kotlin/com/sos/smartopenspace/dto/request/CreateTalkRequestDTO.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/dto/request/CreateTalkRequestDTO.kt
@@ -1,24 +1,24 @@
 package com.sos.smartopenspace.dto.request
 
 import com.sos.smartopenspace.domain.Document
-import java.net.URL
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotEmpty
+import java.net.URL
 
 data class CreateTalkRequestDTO(
-    @field:NotEmpty(message = "Ingrese un nombre")
-    @field:NotBlank(message = "Nombre no puede ser vacío")
-    val name: String,
+  @field:NotEmpty(message = "Ingrese un nombre")
+  @field:NotBlank(message = "Nombre no puede ser vacío")
+  val name: String,
 
-    val description: String = "",
+  val description: String = "",
 
-    val meetingLink: URL? = null,
+  val meetingLink: URL? = null,
 
-    val trackId: Long? = null,
+  val trackId: Long? = null,
 
-    @field:Valid
-    val documents: Set<Document> = emptySet(),
+  @field:Valid
+  val documents: Set<Document> = emptySet(),
 
-    val speakerName: String? = null
+  val speakerName: String? = null
 )

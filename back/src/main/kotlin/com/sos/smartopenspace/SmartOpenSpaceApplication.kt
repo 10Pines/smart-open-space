f282a7d4a6e9af215fc7f4754155ff6b03d951da
Jose Cassano <cassanojoseluis@gmail.com>
2025-10-11 14:16:47 -0300

    add local cache with spring boot and caffeine impl at get schedule assigned slots (with evict when updates that data)

--- back/src/main/kotlin/com/sos/smartopenspace/SmartOpenSpaceApplication.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/SmartOpenSpaceApplication.kt
@@ -2,8 +2,10 @@ package com.sos.smartopenspace
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class SmartOpenSpaceApplication
 
 fun main(args: Array<String>) {

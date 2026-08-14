45e3c16e222d491f5c08d906d2d397d4607c6ce5
Nahuel Garbezza <ngarbezza@users.noreply.github.com>
2026-08-14 10:32:26 -0300

    build(deps): agrupado de actualizaciones seguras (2026-08-04) (#565)
    * build(deps): bump de.undercouch.download from 5.6.0 to 5.7.0 in /back
    
    Bumps [de.undercouch.download](https://github.com/michel-kraemer/gradle-download-task) from 5.6.0 to 5.7.0.
    - [Release notes](https://github.com/michel-kraemer/gradle-download-task/releases)
    - [Commits](https://github.com/michel-kraemer/gradle-download-task/compare/5.6.0...5.7.0)
    
    ---
    updated-dependencies:
    - dependency-name: de.undercouch.download
      dependency-version: 5.7.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump kotlinVersion from 2.3.10 to 2.4.10 in /back
    
    Bumps `kotlinVersion` from 2.3.10 to 2.4.10.
    
    Updates `jvm` from 2.3.10 to 2.4.10
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.10...v2.4.10)
    
    Updates `plugin.spring` from 2.3.10 to 2.4.10
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.10...v2.4.10)
    
    Updates `plugin.jpa` from 2.3.10 to 2.4.10
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.10...v2.4.10)
    
    Updates `plugin.allopen` from 2.3.10 to 2.4.10
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.10...v2.4.10)
    
    ---
    updated-dependencies:
    - dependency-name: jvm
      dependency-version: 2.4.10
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: plugin.spring
      dependency-version: 2.4.10
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: plugin.jpa
      dependency-version: 2.4.10
      dependency-type: direct:production
      update-type: version-update:semver-minor
    - dependency-name: plugin.allopen
      dependency-version: 2.4.10
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump actions/setup-java from 5 to 5.6.0
    
    Bumps [actions/setup-java](https://github.com/actions/setup-java) from 5 to 5.6.0.
    - [Release notes](https://github.com/actions/setup-java/releases)
    - [Commits](https://github.com/actions/setup-java/compare/v5...v5.6.0)
    
    ---
    updated-dependencies:
    - dependency-name: actions/setup-java
      dependency-version: 5.6.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump fast-uri
    
    Bumps the npm_and_yarn group with 1 update in the /front directory: [fast-uri](https://github.com/fastify/fast-uri).
    
    
    Updates `fast-uri` from 3.1.3 to 3.1.5
    - [Release notes](https://github.com/fastify/fast-uri/releases)
    - [Commits](https://github.com/fastify/fast-uri/compare/v3.1.3...v3.1.5)
    
    ---
    updated-dependencies:
    - dependency-name: fast-uri
      dependency-version: 3.1.5
      dependency-type: indirect
      dependency-group: npm_and_yarn
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -2,11 +2,11 @@ import de.undercouch.gradle.tasks.download.Download
 
 plugins {
   base
-  val kotlinVersion = "2.3.10"
+  val kotlinVersion = "2.4.10"
   id("org.springframework.boot") version "3.3.13"
   id("io.spring.dependency-management") version "1.1.7"
   id("org.flywaydb.flyway") version "11.20.0"
-  id("de.undercouch.download") version "5.6.0"
+  id("de.undercouch.download") version "5.7.0"
   id("org.sonarqube") version "7.2.2.6593"
   kotlin("jvm") version kotlinVersion
   kotlin("plugin.spring") version kotlinVersion
